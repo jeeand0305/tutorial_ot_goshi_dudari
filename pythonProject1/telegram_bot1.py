e968acfd68ea2e47a3667edb7eba4d2c7db8a571
andreytest <jeep030579@gmail.com>
2023-03-14 10:37:22 -0700

    1.cenz.json, cenz.txt добавил для удаления мата 2. telegram_bot1.py работает как эхо на модуле telebot

--- pythonProject1/telegram_bot1.py
+++ pythonProject1/telegram_bot1.py
@@ -1,41 +1,12 @@
-from faile_config_telgr1 import * #open_weather_token
-import requests
+
 import telebot
-from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor
+
 
 # my_test_one_bot
 TOKEN = "6224863591:AAGepg6cRgtv9wh0_Db17_sB_tfD81brgxA"
 
 
 #____________________________________________________________
-bot = Bot(TOKEN)
-dp = Dispatcher(bot)
-
-# понять что робот работает
-async def on_startup(_):
-    print("Bot is online")
-
-# ______________________Клиентская часть-____________________
-@dp.message_handlers(commands=['start', 'help'])
-async def start_command(message:types.Message):
-    try:
-        await bot.send_message(\
-            message.from_user.id, "Привет програмист")
-        await message.delete()
-    except:
-        await message.reply("\
-        Общение с ботом только в ЛС: \
-        \n")
-
-
-
-
-#natyznoy_potolok # назвапние бота
-# @natyznoy_potolok_bot
-# token_telegram =6063224285:AAF3eblLJGQiK9BWFtHyntaKRs7UdARASxQ
-# natyznoy_potolok_grup0 # назвапние grup
 
 
 bot = telebot.TeleBot\
